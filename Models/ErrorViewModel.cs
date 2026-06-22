49cec683da2aec719ef0a2345af0bb593375fd4e
r3d <redietgirma2930@gmail.com>
2026-06-22 01:25:21 -0700

    Initial commit - ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MyFirstNewProject.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
